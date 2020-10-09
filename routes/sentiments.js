4727d859a4faaf912bda61a889612e8c7a540b41
Roger Junior <rog.junior@outlook.com>
2020-10-09 18:06:11 -0300

    Adicionar endpoint Get Single Person e Get All People

--- routes/sentiments.js
+++ routes/sentiments.js
@@ -9,49 +9,115 @@ var s3Bucket = new AWS.S3({ params: { Bucket: "uam-emotions" } });
 let Person = require("../models/person");
 
 router.post("/binary", async (req, res) => {
-  buf = Buffer.from(
-    req.body.imageBinary.replace(/^data:image\/\w+;base64,/, ""),
-    "base64"
-  );
+  try {
+    buf = Buffer.from(
+      req.body.ImageBinary.replace(/^data:image\/\w+;base64,/, ""),
+      "base64"
+    );
 
-  const img = {
-    Key: `${uuidv4()}.jpg`,
-    Body: buf,
-    ContentEncoding: "base64",
-    ContentType: "image/jpeg",
-  };
+    const img = {
+      Key: `${uuidv4()}.jpg`,
+      Body: buf,
+      ContentEncoding: "base64",
+      ContentType: "image/jpeg",
+    };
 
-  s3Bucket.putObject(img, function (err, data) {
-    if (err) {
-      return res.json("Error uploading image").status(500);
-    } else {
-      console.log(`Image uploaded: ${img.Key}`);
+    s3Bucket.putObject(img, function (err, data) {
+      if (err) {
+        return res.json("Error uploading image").status(500);
+      } else {
+        //console.log(`Image uploaded: ${img.Key}`);
 
-      var params = {
-        Attributes: ["ALL"],
-        Image: {
-          S3Object: {
-            Bucket: "uam-emotions",
-            Name: img.Key,
+        var params = {
+          Attributes: ["ALL"],
+          Image: {
+            S3Object: {
+              Bucket: "uam-emotions",
+              Name: img.Key,
+            },
           },
-        },
-      };
+        };
 
-      rekognition.detectFaces(params, function (err, data) {
-        if (err) {
-          console.log(err, err.stack);
-          return res.status(500);
-        } else {
-          data.FaceDetails.forEach((element) => {
-            let person = new Person(element);
-            person.save();
-          });
+        rekognition.detectFaces(params, function (err, data) {
+          if (err) {
+            console.log(err, err.stack);
+            return res.status(500);
+          } else {
+            if (data.FaceDetails.length > 0) {
+              console.log(`${data.FaceDetails.length} people saved on database`)
+              data.FaceDetails.forEach((element) => {
+                let person = new Person(element);
 
-          return res.json(data); // successful response
-        }
-      });
-    }
-  });
+                person.Tag = req.body.Tag;
+                person.ImageURL = img.Key;
+                person.VideoURL = req.body.VideoURL;
+                person.Timestamp = req.body.Timestamp;
+
+                if (req.query.randomize == "true") {
+                  person.Location = {
+                    Lat: parseFloat(req.body.Lat) + (Math.random() - 0.5) / 1000,
+                    Long: parseFloat(req.body.Long) + (Math.random() - 0.5) / 1000,
+                  };
+                } else {
+                  person.Location = {
+                    Lat: parseFloat(req.body.Lat) + (Math.random() - 0.5) / 1000,
+                    Long: parseFloat(req.body.Long) + (Math.random() - 0.5) / 1000,
+                  };
+                }
+
+                person.save();
+              });
+            }
+
+            return res.json(data); // successful response
+          }
+        });
+      }
+    });
+  } catch (error) {
+    return res.status(500);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const person = await Person.findById(req.params.id);
+    return res.json(person);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send();
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 150;
+    const page = parseInt(req.query.page) || 1;
+    const skip = page * limit - limit;
+    const projection = null;
+
+    const options = {
+      sort: {
+        CreationDate: -1,
+      },
+      limit,
+      skip,
+    };
+
+    let count = await Person.find({}, projection).countDocuments();
+    let people = await Person.find({}, projection).setOptions(options).exec();
+
+    return await res.json({
+      total_count: count,
+      total_pages: Math.ceil(count / limit),
+      count: people.length,
+      page: options.page,
+      people,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json(error).status(500).send();
+  }
 });
 
 module.exports = router;
